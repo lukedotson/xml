7fd2ee0d77a9ff0a127dc0537eda26c10d5f61ac
Luke Dotson <luke.dotson@KSD.io>
2018-10-23 14:36:25 -0500

    asdFf

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ while (stores[i]) {
 	var phone = store.elements.filter(obj => obj.name === 'phone');
 	var phoneText = phone[0].elements[0];
 	
-	console.log(phone);
+	console.log(phoneText);
 	i++;
 }
 
