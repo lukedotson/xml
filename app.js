e3f42019630c15e04c854704a4de91cc57b95710
Luke Dotson <luke.dotson@KSD.io>
2018-10-23 11:21:33 -0500

    asdf

--- app.js
+++ app.js
@@ -17,15 +17,15 @@ var xml =
 '    <todo>Work</todo>' +
 '    <todo>Play</todo>' +
 '</note>';
-
+var result = convert.xml2json(xmlContents, {compact: true, spaces: 4});
 var page = 
 '<html>' +
 '<body>' +
-'<textarea rows="20" cols="200">' + xmlContents + '</textarea>' +
+'<textarea rows="40" cols="100">' + result + '</textarea>' +
 '</body>' +
 '</html>';
 
-var result = convert.xml2json(xml, {compact: true, spaces: 4});
+//var result = convert.xml2json(xml, {compact: true, spaces: 4});
 app.get('/', (req, res) => res.send(page));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
