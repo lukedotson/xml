088c27321901a6b8799e58de2db75c3116fd0d9b
Luke Dotson <luke.dotson@KSD.io>
2018-10-23 11:30:36 -0500

    Asdf

--- app.js
+++ app.js
@@ -6,14 +6,8 @@ var fs = require('fs');
 var xmlContents;
 
 fs.readFile('test.xml', 'utf8', function(err, contents) {
-	if (contents) {
-		xmlContents = contents;
-	} else {
-		xmlContents = err;
-	}
+	xmlContents = contents;
 });
-
-console.log(xmlContents);
 
 var convert = require('xml-js');
 var xml =
@@ -23,15 +17,8 @@ var xml =
 '    <todo>Work</todo>' +
 '    <todo>Play</todo>' +
 '</note>';
-//var result = convert.xml2json(xmlContents, {compact: true, spaces: 4});
-var page = 
-'<html>' +
-'<body>' +
-'<textarea rows="40" cols="100">' + '</textarea>' +
-'</body>' +
-'</html>';
 
-//var result = convert.xml2json(xml, {compact: true, spaces: 4});
-app.get('/', (req, res) => res.send(page));
+var result = convert.xml2json(xml, {compact: true, spaces: 4});
+app.get('/', (req, res) => res.send(xmlContents));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
