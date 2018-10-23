8f7713d0bd4acf101db7792f4d89cab6e34e8f30
Luke Dotson <luke.dotson@KSD.io>
2018-10-23 13:29:53 -0500

    asdf

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var store = resultjs.elements[0].elements[0].elements;
 
 let phone = store.filter(obj => obj.name === 'phone');
 
-console.log(phone.elements[0]);
+console.log(phone[0].elements[0]);
 
 var xml =
 '<?xml version="1.0" encoding="utf-8"?>' +
