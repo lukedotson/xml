b56314d1fee49a8c351e223b2f9d8b0fca19f014
Luke Dotson <luke.dotson@KSD.io>
2018-10-23 14:25:17 -0500

    Asdf

--- app.js
+++ app.js
@@ -19,39 +19,31 @@ var resultjs = convert.xml2js(xmlContents, {ignoreComment: true, alwaysChildren:
 //console.log(resultjs);
 
 var root = resultjs.elements.filter(obj => obj.name === 'stores');
-console.log(root);
-var stores = root[0].elements.filter(obj => obj.name === 'store');
+//console.log(root);
+//var stores = root[0].elements.filter(obj => obj.name === 'store');
 
 var i=0;
-while (stores[i]) {
+//while (stores[i]) {
 	//var store = stores[i].elements.filter(obj => obj.name == 'store')
 	//var store = stores[i];
 	//var phone = store.elements.filter(obj => obj.name === 'phone');
 	//var phoneText = phone[0].elements.filter(obj => obj.name === 'text');
 	
 	//console.log(phoneText);
-	i++;
-}
+	//i++;
+//}
 
-console.log(i);
+//console.log(i);
 
-var xml =
-'<?xml version="1.0" encoding="utf-8"?>' +
-'<note importance="high" logged="true">' +
-'    <title>Happy</title>' +
-'    <todo>Work</todo>' +
-'    <todo>Play</todo>' +
-'</note>';
-
-var page = 
+/*var page = 
 '<html>' +
 '<body>' +
 '<p>' + resultjs.elements[0].elements[0] + '</p>' +
 '<textarea rows="40" cols="100">' + xmlContents + '</textarea>' +
 '<textarea rows="40" cols="100">' + result.toString() + '</textarea>' +
 '</body>' +
-'</html>';
+'</html>';*/
 
-app.get('/', (req, res) => res.send(page));
+app.get('/', (req, res) => res.send("happy"));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
