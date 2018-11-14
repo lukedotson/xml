0ad8a7de39f944a9214c40a9251038205d6938bd
Luke Dotson <luke.dotson@KSD.io>
2018-11-14 11:59:55 -0600

    adf

--- zip.js
+++ zip.js
@@ -30,7 +30,7 @@ while (stores[i]) {
 	var storeID = store.attributes['store-id'];
 	var latString = JSON.stringify(latitude);
 	//storeID = storeID.replace('&', '&amp;');
-	console.log("store-id: " + storeID + " " + "Latitude: " + latitude.elements);
+	console.log("store-id: " + storeID + " " + "Latitude: " + latitude[0].elements);
 	console.log(latString);
 	store.attributes['store-id'] = storeID;
 	
